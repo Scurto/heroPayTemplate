3dafcbc61c87319cd07f3269f0fb068807a018df
ustymenkoy <ustymenkoy@digicode.net>
2018-09-26 17:55:46 +0300

    added json logger

---
+++ zuul-server/src/main/java/com/heropay/zuulserver/ZuulPreFilter.java
@@ -0,0 +1,44 @@
+package com.heropay.zuulserver;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class ZuulPreFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(ZuulPreFilter.class);
+//    private static Logger log = LoggerFactory.getLogger("jsonLogger");
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        ctx.addZuulRequestHeader("Test", "TestSample");
+
+        HttpServletRequest request = ctx.getRequest();
+
+//        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        log.info("Debug message");
+        return null;
+    }
+}
