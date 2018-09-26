6a0f2b09ad25781f61fdb0eb83c0380c061cc021
ustymenkoy <ustymenkoy@digicode.net>
2018-09-26 14:07:20 +0300

    added config logic

--- core-service/src/main/java/com/heropay/coreservice/RestCoreTemplate.java
+++ core-service/src/main/java/com/heropay/coreservice/RestCoreTemplate.java
@@ -1,5 +1,6 @@
 package com.heropay.coreservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest")
 public class RestCoreTemplate {
 
+    @Value("${user}")
+    private String user;
+
+    @Value("${password}")
+    private String password;
+
     @GetMapping()
     public String message() {
-        return "Hello From CoreRest";
+        return "Hello From CoreRest -> " + user + " | " + password;
     }
 }
